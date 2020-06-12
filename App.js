65d96e2d8d95882451862e4c7864ead8f8300fa0
Bob Hunt <bob.hunt.email@gmail.com>
2020-06-12 16:26:44 -0400

    Initial commit. Followed reactnative.dev and reactnavigation.org docs to create a react native application with screen navigation capabilities (currently only one screen exists).

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+// The above line MUST be the initial like of code for this file.
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        Trelliss Home Screen
+      </Text>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
