25a7b9cc1fb3c70d37789887acbc37fec4009dd0
Bob Hunt <bob.hunt.email@gmail.com>
2020-06-17 23:32:56 -0400

    Added a video screen with a generic looping video on auto-play.

---
+++ components/videoItem.js
@@ -0,0 +1,10 @@
+<Video
+  source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+  rate={1.0}
+  volume={1.0}
+  isMuted={false}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 300, height: 300 }}
+/>
